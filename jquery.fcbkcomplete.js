2f1878dee133480debf7ce89f083c0745d6bc8db
KeiichiroUi <keiichiro.ui@gmail.com>
2011-06-28 10:03:29 +0900

    fixing encoding

--- jquery.fcbkcomplete.js
+++ jquery.fcbkcomplete.js
@@ -83,9 +83,12 @@
           if (option.hasClass("selected")) {
             var id = addItem(option.text(), option.val(), true, 
 			     option.hasClass("locked"));
-            temp_elem.append('<option value="'+option.val()+
-			     '" selected="selected" id="opt_'+id+
-			     '"class="selected">'+option.text()+'</option>');
+            var opt = $('<option>').val(opiton.val()).attr('id', 'opt_'+id)
+              .attr('selected', 'selected').addClass('selected').text(option.text());
+
+            //temp_elem.append('<option value="'+option.val()+
+	    //		     '" selected="selected" id="opt_'+id+
+	    //		     '"class="selected">'+option.text()+'</option>');
           }
         });
         
